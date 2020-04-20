9bc3c918a7cca535ec0d9830934c8a9b19544491
Oier Ajenjo <oierajenjo@opendeusto.es>
2020-04-20 15:34:42 +0200

    Commit 9 Function error corrected

--- manast_site/functions.py
+++ manast_site/functions.py
@@ -2,10 +2,10 @@ from manast_database.models import Expense, Sale
 
 
 def get_all_sales(shop):
-    sales = Sale.objects.get(shop=shop)
+    sales = Sale.objects.filter(shop=shop)
     return sales
 
 
 def get_all_expenses(shop):
-    expenses = Expense.objects.get(shop=shop)
+    expenses = Expense.objects.filter(shop=shop)
     return expenses
