2ecf46727436a136e73996139fbe87de7f0d4d81
Oier Ajenjo <oierajenjo@opendeusto.es>
2020-04-20 13:37:19 +0200

    Commit 7 Models and Views

--- manast_database/models.py
+++ manast_database/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from manast_site.models import Profile, Shop
+
 TYPE = [
     (1, _("Employee")),
     (2, _("Clothes")),
@@ -13,10 +15,11 @@ TYPE = [
 
 
 class Item(models.Model):
-    item_id = models.CharField(max_length=16, verbose_name=_("item ID"), related_name='item ID', unique=True,
-                               on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, verbose_name=_("name"), related_name='name')
-    type = models.IntegerField(choices=TYPE, verbose_name=_("type"))
+    name = models.CharField(max_length=30, verbose_name=_("name_item"))
+    type = models.IntegerField(choices=TYPE, verbose_name=_("type_item"))
+    in_inventory = models.IntegerField(blank=True, verbose_name=_("in_inventory_item"))
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -27,8 +30,12 @@ class Item(models.Model):
 
 
 class Expense(models.Model):
-    item = models.ForeignKey(Item, verbose_name=_("item"), related_name='item', on_delete=models.CASCADE)
-    cost = models.FloatField(verbose_name=_("cost"))
+    item = models.ManyToManyField(Item)
+    quantity = models.IntegerField(verbose_name=_("quantity_expense"))
+    cost = models.FloatField(verbose_name=_("cost_expense"))
+    date = models.DateField(verbose_name=_("date_expense"))
+
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("Expense")
@@ -36,11 +43,13 @@ class Expense(models.Model):
 
 
 class Sale(models.Model):
-    item = models.ForeignKey(Item, verbose_name=_("item"), related_name='item', on_delete=models.CASCADE)
-    quantity = models.IntegerField(verbose_name=_("quantity"))
-    price = models.FloatField(verbose_name=_("price"))
-    cost = models.FloatField(verbose_name=_("cost"))
-    date = models.DateField(verbose_name=_("date"), related_name='date')
+    item = models.ManyToManyField(Item)
+    quantity = models.IntegerField(verbose_name=_("quantity_sale"))
+    price = models.FloatField(blank=True, verbose_name=_("price_sale"))
+    cost = models.FloatField(verbose_name=_("cost_sale"))
+    date = models.DateField(verbose_name=_("date_sale"))
+
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("Sale")
--- manast_site/models.py
+++ manast_site/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
@@ -15,6 +16,7 @@ WEEKDAYS = [
 
 
 class MediaFileSystemStorage(FileSystemStorage):
+
     def get_available_name(self, name, max_length=None):
         if max_length and len(name) > max_length:
             raise (Exception("The file is bigger than supported"))
@@ -28,27 +30,24 @@ class MediaFileSystemStorage(FileSystemStorage):
 
 class OpeningTime(models.Model):
     weekday = models.IntegerField(choices=WEEKDAYS, unique=True, verbose_name=_("weekday"))
-    from_hour = models.TimeField(verbose_name=_("from_hour"), related_name='from_hour')
-    rest_hour = models.TimeField(blank=True, verbose_name=_("rest_hour"), related_name='rest_hour')
-    comeback_hour = models.TimeField(blank=True, verbose_name=_("comeback_hour"), related_name='comeback_hour')
-    to_hour = models.TimeField(verbose_name=_("to_hour"), related_name='to_hour')
-
-
-class Holiday(models.Model):
-    holiday = models.DateField(verbose_name=_("holiday"), related_name='holiday')
+    from_hour = models.TimeField(blank=True)
+    rest_hour = models.TimeField(blank=True)
+    comeback_hour = models.TimeField(blank=True)
+    to_hour = models.TimeField(blank=True)
 
 
 class Shop(models.Model):
-    ID = models.CharField(max_length=20, unique=True, verbose_name=_("shop_id"), related_name='shop_id',
-                          on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, verbose_name=_("name"), related_name='name', on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='shop', blank=True, default="shop/shop.png", verbose_name=_("shop_photo"),
-                              related_name='shop_photo', storage=MediaFileSystemStorage())
+    name = models.CharField(max_length=30, verbose_name=_("name_shop"))
+    photo = models.ImageField(upload_to='shop', blank=True, default="shop/shop.png", verbose_name=_("photo_shop"),
+                              storage=MediaFileSystemStorage())
 
-    direction = models.CharField(max_length=50, blank=True, verbose_name=_("direction"), related_name='direction')
-    postal_code = models.IntegerField(max_length=5, blank=True, verbose_name=_("postal_code"))
-    opening_times = models.ManyToManyField(OpeningTime, verbose_name=_("opening_times"), related_name='opening_times')
-    holidays = models.ManyToManyField(Holiday, verbose_name=_("holidays"), related_name='holidays')
+    direction = models.CharField(max_length=50, blank=True, verbose_name=_("direction_shop"))
+
+    opening_times = models.ManyToManyField(OpeningTime, verbose_name=_("opening_times_shop"),
+                                           related_name='opening_times_shop', blank=True)
+    zip_code = models.CharField(max_length=5, blank=True, verbose_name=_("zip_code_shop"))
+
+    # holidays = ArrayField(models.DateField(blank=True), blank=True)
 
     def __str__(self):
         return self.name
@@ -58,16 +57,21 @@ class Shop(models.Model):
         verbose_name_plural = _("Shops")
 
 
+# class Holiday(models.Model):
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     holiday = models.DateField(blank=True)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True, verbose_name=_("user"), related_name='user',
                                 on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars', blank=True, default="avatars/user.png",
-                               verbose_name=_("avatar"), related_name='avatar', storage=MediaFileSystemStorage())
+                               verbose_name=_("avatar_user"), storage=MediaFileSystemStorage())
 
-    direction = models.CharField(max_length=50, blank=True, verbose_name=_("direction"), related_name='direction')
-    postal_code = models.IntegerField(max_length=5, blank=True, verbose_name=_("postal_code"))
+    direction = models.CharField(max_length=50, blank=True, verbose_name=_("direction_user"))
+    zip_code = models.CharField(max_length=5, blank=True, verbose_name=_("zip_code_user"))
 
-    shops = models.ManyToManyField(Shop, verbose_name=_("shops"), related_name='shops', on_delete=models.CASCADE)
+    shops = models.ManyToManyField(Shop, blank=True, verbose_name=_("shops_user"))
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
