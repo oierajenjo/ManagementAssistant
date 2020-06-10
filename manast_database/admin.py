f4e2083b89f434cff3b21cfa95984144d9e3f5e7
Oier Ajenjo <oierajenjo@opendeusto.es>
2020-06-11 00:25:28 +0200

    Commit 16 Admin

--- manast_database/admin.py
+++ manast_database/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from manast_database.models import *
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ['name']
+    list_display = ['name']
+    # search_fields = ['']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'category', 'shop')
+    list_display = ('name', 'category', 'shop')
+    # search_fields = ['']
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_filter = ('item', 'shop', 'date')
+    list_display = ('shop', 'date')
+    # search_fields = ['']
+
+
+class SalesAdmin(admin.ModelAdmin):
+    list_filter = ('item', 'shop', 'date')
+    list_display = ('shop', 'date')
+
+
+# search_fields = ['']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Sale, SalesAdmin)
